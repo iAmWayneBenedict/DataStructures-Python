1f0b8e7f18019ff552031abacfc51fc3dfdcc8db
iAmWayne <iraolawayne@gmail.com>
2022-07-17 12:29:29 +0800

    added Doubly linkedlist

--- Doubly_LinkedList.py
+++ Doubly_LinkedList.py
@@ -1,7 +1,7 @@
 from Node import Node
 
 
-class Singly_LinkedList:
+class Doubly_LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
@@ -21,52 +21,69 @@ class Singly_LinkedList:
 
         return itr
 
+    def insert_first(self, data):
+        node = Node(data)
+
+        if self.is_empty():
+            self.head = node
+            self.tail = node
+        else:
+            node.next = self.head
+            self.head.prev = node
+            self.head = node
+
     def insert_last(self, data):
         node = Node(data)
         if self.is_empty():
             self.head = node
             self.tail = node
         else:
-            temp = self.tail
-            temp.next = node
-            self.tail = temp.next
-
-    def insert_first(self, data):
-        node = Node(data)
-        if self.is_empty():
-            self.head = node
-            self.tail = node
-        else:
-            temp = self.head
-            self.head = node
-            self.head.next = temp
-
-    def print(self):
-        temp = self.head
-        while temp is not None:
-            print(temp.data)
-            temp = temp.next
+            node.prev = self.tail
+            self.tail.next = node
+            self.tail = self.tail.next
 
     def remove_first(self):
         if self.is_empty():
             print("Empty List")
             return None
 
-        temp = self.head.next
-        self.head = None
-        self.head = temp
+        self.head = self.head.next
+        self.head.prev = None
 
     def remove_last(self):
         if self.is_empty():
             print("Empty List")
             return None
 
-        current_node = self.head
-        while current_node.next.next is not None:
-            current_node = current_node.next
+        self.tail = self.tail.prev
+        self.tail.next = None
 
-        current_node.next = None
-        self.tail = current_node
+    def insert_at(self, data, pos):
+        if self.is_empty():
+            print("Empty List")
+            return None
+
+        if pos > self.length():
+            print("Position out of range")
+            return None
+
+        node = Node(data)
+        if self.is_empty():
+            self.head = node
+            self.tail = node
+            return None
+
+        current_node = self.head
+        itr = 0
+        while itr + 1 is not pos:
+            current_node = current_node.next
+            itr += 1
+
+        temp = current_node.prev
+        node.next = current_node
+        current_node.prev = node
+        temp.next = node
+        node.prev = temp
 
     def remove_at(self, pos):
         if self.is_empty():
@@ -77,47 +94,27 @@ class Singly_LinkedList:
             print("Position out of range")
             return None
 
-        prev_node = self.head
-        itr = 0
-        while itr + 1 is not pos - 1:
-            prev_node = prev_node.next
-            itr += 1
-
         current_node = self.head
         itr = 0
         while itr + 1 is not pos:
             current_node = current_node.next
             itr += 1
 
-        prev_node.next = current_node.next
+        current_node.prev.next = current_node.next
+        current_node.next.prev = current_node.prev
+        # print(current_node.prev.data)
         current_node = None
 
-    def insert_at(self, data, pos):
-        if self.is_empty():
-            print("Empty list")
-            return None
+    def print_descending(self):
+        current_node = self.tail
 
-        if pos > self.length():
-            print("Position out of range")
-            return None
+        while current_node is not None:
+            print(current_node.data)
+            current_node = current_node.prev
 
-        node = Node(data)
-        if self.is_empty():
-            self.head = node
-            self.tail = node
-            return None
-
-        prev_node = self.head
-        itr = 0
-        while itr + 1 is not pos - 1:
-            prev_node = prev_node.next
-            itr += 1
-
+    def print_ascending(self):
         current_node = self.head
-        itr = 0
-        while itr + 1 is not pos:
-            current_node = current_node.next
-            itr += 1
 
-        prev_node.next = node
-        node.next = current_node
+        while current_node is not None:
+            print(current_node.data)
+            current_node = current_node.next
